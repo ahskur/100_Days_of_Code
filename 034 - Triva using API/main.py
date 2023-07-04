08674dad0799477c0b68c07851934a4bc973a6de
ahskur <63268217+ahskur@users.noreply.github.com>
2023-07-04 21:25:14 +0200

    Clean up

--- 034 - Triva using API/main.py
+++ 034 - Triva using API/main.py
@@ -3,6 +3,7 @@ from data import question_data
 from quiz_brain import QuizBrain
 from ui import QuizInterface
 
+
 question_bank = []
 for question in question_data:
     question_text = question["question"]
@@ -17,6 +18,3 @@ quiz_ui = QuizInterface(quiz)
 
 while quiz.still_has_questions():
     quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
